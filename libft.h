1b87d4b910e0e6a1efcb5e43b7d79139cf890599
listaro75 <lucien.dacunha@gmail.com>
2024-08-22 15:33:28 +0200

    ft_putchar_fd

--- libft.h
+++ libft.h
@@ -27,6 +27,8 @@ void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
 void	ft_putnbr(int nbr);
+void	ft_putchar_fd(char c, int fd);
+
 
 int	ft_atoi(const char *nptr);
 
