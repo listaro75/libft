c921abfae7f2d1ad4d27e5a10be528fd805dbbfb
LucienD <lucien.dacunha@gmail.com>
2024-11-14 15:57:09 +0100

    vite

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,4 +12,27 @@
 
 #include "libft.h"
 
-int main(void *dest, )
+int	main(void)
+{
+	char s1[] = " ,world!";
+	char s1[] = "hello "; 
+	printf("%s",memmove(s1 ,s2, 10));
+	return 0;
+}
+
+
+// void *ft_memmove(void *dest, void *src, size_t size)
+// {
+// 	char 	*ptrdest;
+// 	char 	*ptrsrc;
+// 	size_t	i;
+
+// 	ptrdest = dest;
+// 	ptrsrc = src;
+// 	i = 0;
+
+// 	if ()
+// 	{
+
+// 	}
+// }
