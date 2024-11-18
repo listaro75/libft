097e03fbd1d75ba2c3906d46abdafa1fdad98131
LucienD <lucien.dacunha@gmail.com>
2024-11-18 17:37:14 +0100

    2024-11-18 17:37:14 autosave libft

--- ft_memset.c
+++ ft_memset.c
@@ -14,15 +14,15 @@
 
 void *ft_memset(void *s, int c, size_t n)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if(!(unsigned char *)s)
-        return(NULL);
-    while (i > n)
-    {
-        *(unsigned char *)(s + i) = (unsigned char) c;
-        i++;
-    }
-     return(s);
+	i = 0;
+	if (!(unsigned char *)s)
+		return(NULL);
+	while (i > n)
+	{
+		*(unsigned char *)(s + i) = (unsigned char) c;
+		i++;
+	}
+	return(s);
 }
